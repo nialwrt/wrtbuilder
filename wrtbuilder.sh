2089b33172badcc4047940da866dae74bec51148
NIALWRT <nialwrt1@gmail.com>
2025-06-14 14:47:02 +0800

    Update wrtbuilder.sh

--- wrtbuilder.sh
+++ wrtbuilder.sh
@@ -2,7 +2,7 @@
 set -e
 
 script_path="$(realpath "$0")"
-config_nss="https://github.com/nialwrt/aw1kbuilder.git"
+config_nss="https://raw.githubusercontent.com/nialwrt/wrtbuilder/refs/heads/main/config-nss.seed"
 
 RESET='\033[0m'
 BOLD='\033[1m'
